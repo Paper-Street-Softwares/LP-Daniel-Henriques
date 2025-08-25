5998df7714fe5930e9e8c8c82eeb680a1bca1b74
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-25 18:46:29 -0300

    fix: cor dos botoões

--- src/components/sections/Faq.jsx
+++ src/components/sections/Faq.jsx
@@ -48,7 +48,7 @@ export default function Faq({ colorMode }) {
           <Paragraphs className={`text-center transition`}>
             <Button
               className="hoover:scale-0 desktop1:hover:scale-10 transition-all duration-300"
-              color="bg-[#0B2E42]"
+              color="bg-buttonColorDark"
               textclassName="text-white"
               label={t("faq.paragraph")}
               colorMode={colorMode}
